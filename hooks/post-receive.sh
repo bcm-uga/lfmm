c41d6159c20361e59a188be5085fbc204f521da0
cayek <kevin.caye@gmail.com>
2017-08-31 18:01:49 +0200

    deploy on krak

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -11,8 +11,8 @@ do
         git --work-tree=$dest --git-dir=/home/cayek/Gits/2017/lfmm.git checkout -f
 	      cd $dest
 	      make lfmm_install
-        source activate MaThese
-	      make lfmm_install
+        # source activate MaThese
+	      # make lfmm_install
     else
         echo "Ref $ref successfully received.  Doing nothing: only the master branch may be deployed on this server."
     fi
