19f62505cbbde43118c0a843d9e6a769948aa526
Olivier Francois <Olivier.Francois@imag.fr>
2017-10-26 16:19:42 +0200

    add data example

---
+++ R/example.R
@@ -0,0 +1,17 @@
+#' Genetic and geographic data for Arabidopsis thaliana
+#'
+#' A dataset containing SNP frequency and geographic data for 170 plant accessions.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item genotype: binary (0 or 1) SNP frequency for 170 accessions (26943 SNPs).
+#'   \item coord: longitude and latitude of individuals
+#'   \item chrpos: genetic map: chromosome and position of each SNP
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name A.thaliana
+#' @usage data(A.thaliana)
+#' @format A list with 3 arguments: genotype, coord, chrpos
+NULL
---
+++ R/example_data.R
@@ -0,0 +1,18 @@
+#' Genetic and phenotypic data for Arabidopsis thaliana
+#'
+#' A dataset containing SNP frequency and simulated phenotypic data for 170 plant accessions.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item genotype: binary (0 or 1) SNP frequency for 170 individuals (26943 SNPs).
+#'   \item phenotype: simulated phenotypic data for 170 individuals.
+#'  \item causal.set: set of indices for causal SNPs.
+#'   \item chrpos: genetic map including chromosome position of each SNP.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name example.data
+#' @usage data(example.data)
+#' @format A list with 4 arguments: genotype, phenotype, causal.set, chrpos
+NULL
