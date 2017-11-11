46fcf326551366fa25780fed24d017ddc3bf5d9c
Olivier Francois <Olivier.Francois@imag.fr>
2017-11-11 20:03:35 +0100

    predict fix

--- R/lfmm.R
+++ R/lfmm.R
@@ -473,7 +473,7 @@ effect_size <- function(Y, X, lfmm.object){
 ##' ##Compare simulated and predicted/fitted phenotypes
 ##' plot(x - mean(x), x.pred, 
 ##'      pch = 19, col = "grey", 
-##'      xlab = "Observed phenotypes (centered)" 
+##'      xlab = "Observed phenotypes (centered)", 
 ##'      y = "Predicted from PRS")
 ##' abline(0,1)
 ##' abline(lm(x.pred ~ scale(x, scale = FALSE)), col = 2)
