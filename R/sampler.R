a352248add85de82656fdae6ba7d7b0f49e71017
Olivier Francois <Olivier.Francois@imag.fr>
2018-01-18 16:56:54 +0100

    bug lfmm_sampler fixed

--- R/sampler.R
+++ R/sampler.R
@@ -5,37 +5,41 @@ SimulatedLfmmDat.builder <- setRefClass("SimulatedLfmmDat", contains = "LfmmDat"
 
 ##' LFMM generative data sampler
 ##'
-##' Sample data with the latent factor model.
+##' Simulate data from the latent factor model.
 ##'
-##' `lfmm_sample()` sample an output matrix Y and primary matrix X such that
+##' `lfmm_sample()` sample a response matrix Y and a primary variable X such that
 ##'
 ##'          Y = U t(V) + X t(B) + Epsilon.
 ##'
-##' U,V, B and Epsilon are sampled with normal multivariate distribution.
+##' U,V, B and Epsilon are simulated according to normal multivariate distributions.
 ##' Moreover U and X are such that `cor(U[,i], X) = cs[i]`.
 ##'
-##' @return A list with sampled data.
-##' @author kevin caye
+##' @return A list with simulated data.
+##' @author kevin caye, olivier francois
 ##' @param n number of observations.
-##' @param p number of output variables.
-##' @param K number of latent variables.
+##' @param p number of response variables.
+##' @param K number of latent variables (factors).
 ##' @param outlier.prop proportion of outlier.
 ##' @param cs correlation with between X and U.
-##' @param sigma standard deviation of the error.
-##' @param B.sd standard deviation of B.
+##' @param sigma standard deviation of residual errors.
+##' @param B.sd standard deviation for the effect size (B).
 ##' @param B.mean mean of B.
-##' @param U.sd standard deviation of U.
-##' @param V.sd standard deviation of V.
+##' @param U.sd standard deviations for K factors.
+##' @param V.sd standard deviations for loadings.
 ##' @export
 ##'
 ##' @examples
 ##'
-##' dat <- lfmm_sampler(n = 100, p = 1000, K = 3,
+##' dat <- lfmm_sampler(n = 100, 
+##'                     p = 1000, 
+##'                     K = 3,
 ##'                     outlier.prop = 0.1,
 ##'                     cs = c(0.8),
 ##'                     sigma = 0.2,
-##'                     B.sd = 1.0, B.mean = 0.0,
-##'                     U.sd = 1.0, V.sd = 1.0)
+##'                     B.sd = 1.0, 
+##'                     B.mean = 0.0,
+##'                     U.sd = 1.0, 
+##'                     V.sd = 1.0)
 lfmm_sampler <- function(n, p, K,
                          outlier.prop,
                          cs,
@@ -56,24 +60,26 @@ lfmm_sampler <- function(n, p, K,
     cs <- c(cs, rep(0, times = K - length(cs)))
   }
 
-  ## sample U and X
-  Sigma <- diag(x = U.sd, nrow = K, ncol = K)
+  ## simulate U and X
+  theta2 <- sum((cs/U.sd)^2) + 0.01
+    
+  Sigma <- diag(x = U.sd^2, nrow = K, ncol = K)
   Sigma <- rbind(Sigma, matrix(cs, nrow = 1))
-  Sigma <- cbind(Sigma, matrix(c(cs, 1.0), ncol = 1))
+  Sigma <- cbind(Sigma, matrix(c(cs, theta2), ncol = 1))
   UX <- MASS::mvrnorm(n, mu = rep(0.0, K + 1), Sigma = Sigma)
   U <- UX[,1:K, drop = FALSE]
   X <- UX[,K + 1, drop = FALSE]
 
-  ## sample V
-  V <- MASS::mvrnorm(p, mu = rep(0.0, K), Sigma = V.sd * diag(K))
+  ## simulate V
+  V <- MASS::mvrnorm(p, mu = rep(0.0, K), Sigma = V.sd^2 * diag(K))
 
 
-  ## sample B
+  ## simulate B
   B <- matrix(0, p, 1)
   B[outlier, 1] <- rnorm(outlier.nb, B.mean, B.sd)
 
   ## sample error
-  Epsilon = MASS::mvrnorm(n, mu = rep(0.0, p), Sigma = sigma * diag(p))
+  Epsilon = MASS::mvrnorm(n, mu = rep(0.0, p), Sigma = sigma^2 * diag(p))
 
   ## syntheses
   Y = U %*% t(V) + X %*% t(B) + Epsilon
